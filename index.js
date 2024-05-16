0ba615fe1d82ec0d946c9703363295fe25c5f3f0
V1kyCh <126974500+V1kyCh@users.noreply.github.com>
2024-05-16 19:59:03 +0300

    js те що знаю

---
+++ index.js
@@ -0,0 +1,31 @@
+let age = 14;
+console.log(age);
+
+const myName = 'Victoria';
+console.log(myName);
+
+let isStudent = 'Ти є студентом?';
+confirm(isStudent);
+const result = confirm(isStudent);
+console.log(result);
+
+const myString = 'Життя красиве, коли кольорове...';
+console.log(myString);
+
+const myNumber = 10;
+const numberResult = myNumber + 10;
+console.log('myNumber -', myNumber)
+console.log('numberResult -', numberResult);
+
+const myNull = null;
+console.log(myNull);
+
+let yourName = 'Enter your name';
+prompt(yourName);
+const resultYourName = prompt(yourName);
+console.log('Привіт,',resultYourName);
+
+let actionСonfirmation = 'Ви підтверджуєте дію?';
+confirm(actionСonfirmation);
+const resultActionСonfirmation = confirm(actionСonfirmation);
+console.log(resultActionСonfirmation);
