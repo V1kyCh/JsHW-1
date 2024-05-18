56d7a81516325b65f4b5af8f58b520f04ebdd287
V1kyCh <126974500+V1kyCh@users.noreply.github.com>
2024-05-18 14:40:56 +0300

    fix

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
 let age = 14;
 console.log(age);
 
-const myName = 'Victoria';
+const myName = "Victoria";
 console.log(myName);
 
-let isStudent = 'Ти є студентом?';
+let isStudent = "Ти є студентом?";
 confirm(isStudent);
 const result = confirm(isStudent);
 console.log(result);
 
-const myString = 'Життя красиве, коли кольорове...';
+const myString = "Життя красиве, коли кольорове...";
 console.log(myString);
 
 const myNumber = 10;
 const numberResult = myNumber + 10;
-console.log('myNumber -', myNumber)
-console.log('numberResult -', numberResult);
+console.log("myNumber -", myNumber);
+console.log("numberResult -", numberResult);
 
 const myNull = null;
 console.log(myNull);
 
-let yourName = 'Enter your name';
+let yourName = "Enter your name";
 prompt(yourName);
 const resultYourName = prompt(yourName);
-console.log('Привіт,',resultYourName);
+console.log("Привіт,", resultYourName);
 
-let actionСonfirmation = 'Ви підтверджуєте дію?';
-confirm(actionСonfirmation);
-const resultActionСonfirmation = confirm(actionСonfirmation);
-console.log(resultActionСonfirmation);
+const confirmation = confirm("Підтвердити?");
+if (confirmation === true) {
+  alert("Дякую за підтвердження!");
+} else {
+  alert("Дію відмінено!");
+}
+
+alert("Ця дія небезпечна!");
+const confirmationAlert = confirm("Підтвердити?");
+if (confirmationAlert === true) {
+  alert("Дякую за підтвердження!");
+} else {
+  alert("Дію відмінено!");
+}
